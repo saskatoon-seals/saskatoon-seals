849df4d6f00fc4b7df28d2246631f2cd64291979
Saskatoon Seals <saskatoon.seals@gmail.com>
2015-06-30 13:37:16 -0600

    -index handler redirects to u-hockey page

--- src/app/handlers/index.py
+++ src/app/handlers/index.py
@@ -3,7 +3,4 @@ from app.handlers import BaseHandler
 
 class Index(BaseHandler):
     def get(self):
-        context = {
-            'text': 'Hello Seals!'
-        }
-        return self.render_response('index.html', **context)
+        return self.redirect('u-hockey')
